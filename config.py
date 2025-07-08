b519e1fb3a06764e92cb65d0fa8e0d2e84ffaad2
Doyin <doyinelugbadebo@gmail.com>
2025-07-08 18:20:03 +0100

    first commit

--- config.py
+++ config.py
@@ -1,35 +1,12 @@
-# Original
-#--------------------------------------------------------------
 import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev_key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'mysql://root:@localhost/flask_bookdb')
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'SQLALCHEMY_DATABASE_URI',
+        'sqlite:///bookdb.sqlite'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
-# # Docker With local MySQL by connecting to host.docker.internal
-# #--------------------------------------------------------------
-# import os
-
-# class Config:
-#     SECRET_KEY = os.getenv('SECRET_KEY', 'dev_key')
-#     SQLALCHEMY_DATABASE_URI = os.getenv(
-#         'SQLALCHEMY_DATABASE_URI',
-#         'mysql://root:@host.docker.internal/flask_bookdb'
-#     )
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-# # With Docker Compose file to spin up both MySQL + Flask together
-# # --------------------------------------------------------------
-# import os
-
-# class Config:
-#     SECRET_KEY = os.getenv('SECRET_KEY', 'dev_key')
-#     SQLALCHEMY_DATABASE_URI = os.getenv(
-#         'SQLALCHEMY_DATABASE_URI',
-#         'mysql://root:password@db:3306/flask_bookdb'
-#     )
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
